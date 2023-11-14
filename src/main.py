9729b40d325be6b917b3e72c8c5274f99302b21e
GregoireLamb <gdelambertye03@gmail.com>
2023-11-14 14:42:12 +0100

    add instance visualization

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.config import Config
 from src.instance_loader import InstanceLoader
 from src.methods.deterministic_construction_heuristic import DeterministicConstructionHeuristic
-
+from src.utils import Visualisation
 
 def load_method(method_name):
     if method_name == 'deterministic_construction_heuristic':
@@ -13,5 +13,7 @@ if __name__ == '__main__':
     instance_loader = InstanceLoader(config)
     instances = instance_loader.load_instances()
 
-    for instance in instances:
-        method
+    # for instance in instances:
+    instance = instances[0]
+    vis = Visualisation(instance)
+    vis.plot_graph()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Instance:
     """
     Class that represents an instance of the S-Plex problem
@@ -6,7 +9,19 @@ class Instance:
         self.s = int(s)
         self.n = int(n)
         self.m = int(m)
-        self.edge_ingo = edge_info
+        self.edge_info = edge_info
 
         # Create additional structures needed
         self.N = list(range(1, self.n + 1))
+
+
+class Visualisation:
+    def __init__(self, instance):
+        self.instance = instance
+
+    def plot_graph(self):
+        G = nx.Graph()
+        G.add_nodes_from(self.instance.N)
+        G.add_edges_from(self.instance.edge_info.keys())
+        nx.draw(G, with_labels=True)
+        plt.show()
