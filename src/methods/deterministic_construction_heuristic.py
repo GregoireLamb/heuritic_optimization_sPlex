c2e8859757d6c8f774b01fd074ea7b8e9186223f
Joan Salvà Soler <jsalvasoler@hotmail.com>
2023-11-15 16:24:35 +0100

    New developments:
    - Implement Solution class
    
    - First version of Deterministic Heuristic (still with non-implmented methods)

--- src/methods/deterministic_construction_heuristic.py
+++ src/methods/deterministic_construction_heuristic.py
@@ -1,9 +1,41 @@
-from pymhlib.solution import SetSolution
+from pymhlib.solution import VectorSolution
+
+from src.utils import Solution
+
 
 class DeterministicConstructionHeuristic:
     def __init__(self, params=None):
-        pass
+        self._instance = None
+        self._x = {}
 
-    def solve(self, instance):
-        pass
+    def solve(self, instance) -> Solution:
+        self._instance = instance
+        self._x = {e: 0 for e in instance.edges}
+        components = set()
+
+        to_add = set(instance.nodes)
+
+        while len(to_add) > 0:
+            best_cost, best_component, best_node = -1e10, -1, -1
+            for i in to_add:
+                # New component
+                cost = self.compute_cost_of_new_component(i)
+                if cost < best_cost:
+                    best_cost = cost
+                    best_component = -1
+                    best_node = i
+                # Existing component
+                for k in components:
+                    cost = self.compute_insertion_cost_to_component(i, k)
+                    if cost < best_cost:
+                        best_cost = cost
+                        best_component = k
+                        best_node = i
+            if best_component == -1:
+                components.add({best_node})
+            else:
+                self.add_to_component(best_node, best_component)
+            to_add.remove(best_node)
+        return Solution(instance, self._x)
+
 
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 class Instance:
     """
     Class that represents an instance of the S-Plex problem
     """
+
     def __init__(self, s, n, m, edge_info):
         self.s = int(s)
         self.n = int(n)
@@ -12,7 +14,27 @@ class Instance:
         self.edge_info = edge_info
 
         # Create additional structures needed
-        self.N = list(range(1, self.n + 1))
+        self.nodes = list(range(1, self.n + 1))
+        self.edges = list(self.edge_info.keys())
+        self.weight = {e: self.edge_info[e][1] for e in self.edges}
+        self.in_instance = {e: self.edge_info[e][0] for e in self.edges}
+
+        self.graph = nx.Graph()
+        self.graph.add_nodes_from(self.nodes)
+        self.graph.add_edges_from(self.edges)
+
+
+class Solution:
+    def __init__(self, instance: Instance, x: dict):
+        self.instance = instance
+        self.x = x  # Dictionary with edge as key and 1 if edge is in final graph, 0 otherwise
+
+    def evaluate(self):
+        obj = 0
+        for edge in self.instance.edges:
+            if self.x[edge] != self.x[edge]:
+                obj += self.instance.weight[edge]
+        return obj
 
 
 class Visualisation:
@@ -21,7 +43,7 @@ class Visualisation:
 
     def plot_graph(self):
         G = nx.Graph()
-        G.add_nodes_from(self.instance.N)
+        G.add_nodes_from(self.instance.nodes)
         G.add_edges_from(self.instance.edge_info.keys())
         nx.draw(G, with_labels=True)
         plt.show()
